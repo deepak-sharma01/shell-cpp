85dc10d6b48e824592da2988f0ba3e0f5e283785
Deepak Sharma <deepaksharmads.1501@gmail.com>
2026-01-21 16:15:20 +0530

    just fun

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
+int main() 
+{  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -13,12 +13,17 @@ int main() {
   
   
   std::string input;
-  while(std::getline(std::cin,input)){
+  while(std::getline(std::cin,input))
     if(input == "exit"){
       return 0;
     }
+   else if(input.rfind("echo ",0)==0) {    
+    std:: cout<<input.substr(5)<<std::endl;
+    std::cout<<"$ ";
+   }
+   else{
     std::cout << input << ": command not found" << std::endl;
     std::cout<<"$ ";
-  }
+   }
   
-}
+  }
