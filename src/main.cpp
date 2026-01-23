e7ae76080f1237a26f5deb012ef77f8550a235e8
Deepak Sharma <deepaksharmads.1501@gmail.com>
2026-01-23 12:47:42 +0530

    6 stage

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
-
-
+#include<cstdlib>
+#include<sstream>
+#include<vector>
+#include<io.h>
+using namespace std;
 
 int main() 
-{  // Flush after every std::cout / std:cerr
+{ 
+
+ 
+  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
 
-
+char* path = getenv("PATH");
 
   // TODO: Uncomment the code below to pass the first stage
 
@@ -35,22 +41,53 @@ while(std::getline(std::cin,input)){
   
    else if(input.rfind("type ",0)==0){
     bool found = false;
+   std:: string cmd = input.substr(5);
+    
     for(auto v :l){
       
-      if(input.substr(5)==v){
+      if(cmd==v){
         std::cout<<v<< " is a shell builtin\n";
         found = true;
       break;
     }
   }
+  if (!found){
+    if(path){
+      
+      
+    std::stringstream ss(path);
+    std::string dir;
+
+    vector<string> exts = {".exe",".bat",".cmd",".com",""};
+
+
+    while(std::getline(ss,dir,';')){
+      for(auto& ext : exts){
+         std::string full = dir + "\\"+ cmd + ext ;
+      if(_access(full.c_str(),0) == 0){
+        std::cout<<input<<" is "<<full<<endl;
+        found = true;
+        break;
+      }
+      }
+      if(found) break;
+    }
+    }
+  
     if(!found){
-      std::cout<<input.substr(5)<<": not found\n";
-     }
+     std:: cout<< cmd << ": not found\n";
+    }
+   
+  }
+    
+    
+ 
    std::cout<<"$ "; 
     } 
   else{
     std::cout << input << ": command not found" << std::endl;
     std::cout<<"$ ";
   }
+
 }
 }
